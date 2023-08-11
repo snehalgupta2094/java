430d5c3384bf446b4484c4790116f979f2044dc8
guptasn <guptasn@vmware.com>
2023-08-11 13:15:09 +0530

    output

---
+++ practice/src/LLD/VotingHackthon/VotingApplication.java
@@ -0,0 +1,39 @@
+package LLD.VotingHackthon;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+//https://leetcode.com/discuss/interview-experience/1739628/atlassian-sse-remote-reject
+public class VotingApplication {
+
+    public static void getWinner(List<List<String>> votes){
+        Map<String, Integer> voteCountMap=new HashMap<>();
+        for(List<String> vote: votes){
+          int weight=vote.size();
+          for(int i=0;i<vote.size();i++){
+              voteCountMap.put(vote.get(i),voteCountMap.getOrDefault(vote.get(i),0)+weight);
+              weight--;
+          }
+        }
+
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(voteCountMap.entrySet());
+        entryList.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+        List<String> sortedVotes = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : entryList) {
+            sortedVotes.add(entry.getKey());
+        }
+
+        System.out.println("Sorted Candidates: " + sortedVotes);
+
+    }
+    public static void main(String[] args) {
+      List<List<String>> votes=new ArrayList<>();
+      votes.add(Arrays.asList("A", "B", "C"));
+      votes.add(Arrays.asList("A", "C", "D"));
+      votes.add(Arrays.asList("D", "A", "C"));
+      getWinner(votes);
+    }
+
+}
