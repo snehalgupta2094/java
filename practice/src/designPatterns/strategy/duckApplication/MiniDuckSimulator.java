1e84cbd67ac0b84fb02b39455ecb5959f28924e7
https://github.com/snehalgupta2094 <snehalgupta2094@gmail.com>
2023-12-17 15:38:28 +0530

    output

--- practice/src/designPatterns/strategy/duckApplication/MiniDuckSimulator.java
+++ practice/src/designPatterns/strategy/duckApplication/MiniDuckSimulator.java
@@ -7,21 +7,25 @@ public class MiniDuckSimulator {
         duck.display();
         duck.performFly();
         duck.performQuack();
+        duck.swim();
         System.out.println("-----------------");
         duck=new RubberDuck();
         duck.display();
         duck.performFly();
         duck.performQuack();
+        duck.swim();
         System.out.println("-----------------");
         duck=new RedheadDuck();
         duck.display();
         duck.performFly();
         duck.performQuack();
+        duck.swim();
         System.out.println("-----------------");
         duck=new DecoyDuck();
         duck.display();
         duck.performFly();
         duck.performQuack();
+        duck.swim();
         System.out.println("-----------------");
     }
 }
